2f770686470e949b1393e90e7f4329a15da5ee92
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-01 20:09:11 -0300

    first project
    First project: Hello World

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
