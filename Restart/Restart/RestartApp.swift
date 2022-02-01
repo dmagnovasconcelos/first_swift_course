d22da1fb8e846fcd48ad257beb3c05712866c2d4
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-01 20:40:08 -0300

    :sparkles: Add Restart project
    Adding new Restart project.

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 01/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
