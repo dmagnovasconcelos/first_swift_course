cafba6567543c2ff3ed011c25a6926819f4f89bf
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-02 01:22:45 -0300

    :sparkles: feat(HomeView) Adding Animation
    Adding Animation

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct HomeView: View {
     // MARK: PROPERTY
+    
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+    
+    @State private var isAnimating: Bool = false
     
     // MARK: BODY
     var body: some View {
@@ -25,6 +28,13 @@ struct HomeView: View {
                     .resizable()
                     .scaledToFit()
                 .padding()
+                .offset(y: isAnimating ? 35 : -35)
+                .animation(
+                    Animation
+                        .easeInOut(duration: 4)
+                        .repeatForever()
+                    , value: isAnimating
+                )
             }
             
             // MARK: - CENTER
@@ -41,7 +51,9 @@ struct HomeView: View {
             Spacer()
             
             Button(action: {
-                isOnboardingViewActive = true
+                withAnimation {
+                    isOnboardingViewActive = true
+                }
             }) {
                 Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                     .imageScale(.large)
@@ -53,8 +65,12 @@ struct HomeView: View {
             .buttonStyle(.borderedProminent)
             .buttonBorderShape(.capsule)
             .controlSize(.large)
-            
         }//: VSTACK
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                isAnimating = true
+            })
+        })
     }
 }
 
