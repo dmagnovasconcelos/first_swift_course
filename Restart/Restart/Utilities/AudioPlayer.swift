d6abcec0068ecab4063e88a53544de655173984b
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-02 01:49:10 -0300

    :sparkles: feat(AudioPlayer) Add function play sound

---
+++ Restart/Restart/Utilities/AudioPlayer.swift
@@ -0,0 +1,23 @@
+//
+//  AudioPlayer.swift
+//  Restart
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 02/02/22.
+//
+
+import Foundation
+import AVFoundation
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("Could not play the sound file.")
+        }
+    }
+}
+
