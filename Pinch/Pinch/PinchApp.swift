902475e05bcc512a0bc7c9af20bb7728594e3992
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-03 21:03:42 -0300

    :sparkles: feat: Adding Pinch project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 03/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 03/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
