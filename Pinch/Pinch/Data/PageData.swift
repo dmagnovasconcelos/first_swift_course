1e86d258a4804ae80f03558905b40eb77c3f98f9
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-04 00:59:32 -0300

    :sparkles: feat: Finish Pinch project

---
+++ Pinch/Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 04/02/22.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
---
+++ Pinch/Pinch/View/ControlImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 03/02/22.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
