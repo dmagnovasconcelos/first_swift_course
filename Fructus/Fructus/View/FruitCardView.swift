8de5884fa4d2f70e8ddfef3a3d3d4a827c5d07c1
Danilo Magno de Oliveira Vasconcelos <dmagno.vasconcelos@gmail.com>
2022-02-28 12:08:38 -0300

    :truck: Organizer project

---
+++ Fructus/Fructus/View/FruitCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Danilo Magno de Oliveira Vasconcelos on 28/02/22.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+    }
+}
